d3f3c7b77055bbd4f95ac0655c94f18215680dfc
ZeTomCreusot <tom.creusot@epfedu.fr>
2020-03-10 08:59:32 +0100

    Initial commit

---
+++ app/src/main/java/com/example/beta_projet/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.beta_projet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Spinner monSpinner = (Spinner) findViewById(R.id.spinner1);
+        ArrayAdapter<CharSequence> monAdapter = ArrayAdapter.createFromResource (this,
+                R.array.tab_assos, android.R.layout.simple_spinner_item);
+        monAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        monSpinner.setAdapter(monAdapter);
+
+        // 5) Association de adapter au spinner
+        monSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            // View : Un objet de type view est associé au choix fait dans le spinner
+            public void onItemSelected(AdapterView<?> spinner_pokemon, View view, int position, long
+                    id) {
+                // on met ici ce qu’il faut faire lorsqu’un item a ete selectionne
+                System.out.println("un item a été selectionné");
+                String text = spinner_pokemon.getSelectedItem().toString();
+                System.out.println("un item a été selectionné : "+text);
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                // on met ici ce qu’il faut faire lorsqu’aucun item n’est selectionne
+                System.out.println("aucun item n’est selectionné");
+            }
+
+
+        });
+
+    }
+}
