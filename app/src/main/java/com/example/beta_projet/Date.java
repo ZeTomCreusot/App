6cfa5561623d58cc56ff166c8096da8346a66e3d
LuluHrs <lucie.hours@gmail.com>
2020-03-28 18:07:23 +0100

    Version 2803

--- app/src/main/java/com/example/beta_projet/Date.java
+++ app/src/main/java/com/example/beta_projet/Date.java
@@ -1,6 +1,7 @@
 package com.example.beta_projet;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.widget.ListViewAutoScrollHelper;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -9,61 +10,67 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.Spinner;
 
 public class Date extends AppCompatActivity {
     private Button bouttonretour;
     private EditText produit;
+    public ListView ListeProduits;
+    public String nouveauProduit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_date);
 
-        produit = (EditText)findViewById(R.id.produit2);
 
-        Spinner monSpinner = (Spinner) findViewById(R.id.spinner2);
+        Spinner monSpinner = (Spinner) findViewById(R.id.ListeAsso);
         ArrayAdapter<CharSequence> monAdapter = ArrayAdapter.createFromResource (this,
                 R.array.tab_assos, android.R.layout.simple_spinner_item);
         monAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         monSpinner.setAdapter(monAdapter);
-//test
-        // 5) Association de adapter au spinner
         monSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
-            // View : Un objet de type view est associé au choix fait dans le spinner
-            public void onItemSelected(AdapterView<?> spinner_pokemon, View view, int position, long
+            public void onItemSelected(AdapterView<?> spinner_Asso, View view, int position, long
                     id) {
-                // on met ici ce qu’il faut faire lorsqu’un item a ete selectionne
-                System.out.println("un item a été selectionné");
-                String text = spinner_pokemon.getSelectedItem().toString();
-                System.out.println("un item a été selectionné : "+text);
+                String text = spinner_Asso.getSelectedItem().toString();
+                if (text.equals("BDS")) {
+                    afficherListeProduitsBDS();
+                    produit = (EditText)findViewById(R.id.produitEntre);
+                    nouveauProduit=produit.getText().toString();
+                }
             }
             @Override
             public void onNothingSelected(AdapterView<?> parent) {
-                // on met ici ce qu’il faut faire lorsqu’aucun item n’est selectionne
-                System.out.println("aucun item n’est selectionné");
             }
 
-
-
         });
-
-
-
         bouttonretour = (Button) findViewById(R.id.button_retour);
         bouttonretour.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                visualiser_la_suite();
-
+                visualiser_la_suite_PageA();
             }
         });
     }
-    private void visualiser_la_suite()
+    private void visualiser_la_suite_PageA()
     {
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
+
+    private void afficherListeProduitsBDS(){
+        ListeProduits= findViewById(R.id.ListeProduits);
+        String[] listeProduits=new String[]
+                {
+                        "Pain",
+                        "Lait",
+                        "Jus",
+                };
+        ArrayAdapter<String> arrayAdapteur= new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,listeProduits);
+        ListeProduits.setAdapter(arrayAdapteur);
+    }
+
 
 }
