7a73115ecacf50982ec72e340917a5754e2cd1e6
ZeTomCreusot <tom.creusot@epfedu.fr>
2020-04-08 10:17:09 +0200

    Ajout boutons dan sle main

--- app/src/main/java/com/example/beta_projet/Date2.java
+++ app/src/main/java/com/example/beta_projet/Date2.java
@@ -10,11 +10,57 @@ import android.widget.Button;
 public class Date2 extends AppCompatActivity {
     private Button button_retour2;
 
+    // creation des references boutons présents dans le layout
+    // déclaration, sans initialisation
+    Button boutonAjouteEtudiant;
+    Button boutonListeEtudiants;
+    Button boutonEnleveEtudiant;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_date2);
+
+
+
+
+        // association de nos references avec les objets du layout
+        boutonAjouteEtudiant = (Button) findViewById(R.id.btn_ajout_element);
+        boutonListeEtudiants = (Button) findViewById(R.id.btn_voir_liste);
+        boutonEnleveEtudiant = (Button) findViewById(R.id.btn_supprimer_element);
+
+        // ajout des écouuteurs : quand on va cliqueur sur un bounton on va charger l'activité associée
+        // ajout de l'écouteur sur boutonAjouterEtudiant
+        boutonAjouteEtudiant.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // ovuerture de l'activité AjoutEtudiant
+                Intent intent = new Intent(Date2.this, AjoutEtudiant.class);
+                startActivity(intent);
+            }
+        });
+
+        // ajout de l'écouteur sur boutonAjouterEtudiant
+        boutonListeEtudiants.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // ovuerture de l'activité AjoutEtudiant
+                Intent intent = new Intent(Date2.this, ListeEtudiants.class);
+                startActivity(intent);
+            }
+        });
+
+        // ajout de l'écouteur sur boutonAjouterEtudiant
+        boutonEnleveEtudiant.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // ovuerture de l'activité AjoutEtudiant
+                Intent intent = new Intent(Date2.this, SupprimerEtudiant.class);
+                startActivity(intent);
+            }
+        });
+
 
 
         button_retour2 = (Button) findViewById(R.id.button_retour2);
--- app/src/main/java/com/example/beta_projet/MainActivity.java
+++ app/src/main/java/com/example/beta_projet/MainActivity.java
@@ -14,11 +14,7 @@ import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
-    // creation des references boutons présents dans le layout
-    // déclaration, sans initialisation
-    Button boutonAjouteEtudiant;
-    Button boutonListeEtudiants;
-    Button boutonEnleveEtudiant;
+
 
 
     private Button btn;
@@ -34,41 +30,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // association de nos references avec les objets du layout
-        boutonAjouteEtudiant = (Button) findViewById(R.id.btn_ajout_element);
-        boutonListeEtudiants = (Button) findViewById(R.id.btn_voir_liste);
-        boutonEnleveEtudiant = (Button) findViewById(R.id.btn_supprimer_element);
-
-        // ajout des écouuteurs : quand on va cliqueur sur un bounton on va charger l'activité associée
-        // ajout de l'écouteur sur boutonAjouterEtudiant
-        boutonAjouteEtudiant.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // ovuerture de l'activité AjoutEtudiant
-                Intent intent = new Intent(MainActivity.this, AjoutEtudiant.class);
-                startActivity(intent);
-            }
-        });
-
-        // ajout de l'écouteur sur boutonAjouterEtudiant
-        boutonListeEtudiants.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // ovuerture de l'activité AjoutEtudiant
-                Intent intent = new Intent(MainActivity.this, ListeEtudiants.class);
-                startActivity(intent);
-            }
-        });
-
-        // ajout de l'écouteur sur boutonAjouterEtudiant
-        boutonEnleveEtudiant.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // ovuerture de l'activité AjoutEtudiant
-                Intent intent = new Intent(MainActivity.this, SupprimerEtudiant.class);
-                startActivity(intent);
-            }
-        });
 
 
 
